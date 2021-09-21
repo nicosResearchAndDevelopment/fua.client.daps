59e0d6167aefbd8ee76d58b95c821ed5a7d0579c
Simon Petrac <pfoerdie@gmail.com>
2021-09-21 18:25:23 +0200

    wip: daps client

--- next/ids.client.daps.js
+++ next/ids.client.daps.js
@@ -3,24 +3,39 @@ const
     {KeyObject}            = require('crypto'),
     util                   = {
         ..._util,
-        assert:       _util.Assert('ids.client.daps'),
-        isSKI:        _util.StringValidator(/^(?:[0-9a-f]{2}(?::|$)){20}(?=$)/i),
-        isAKI:        _util.StringValidator(/^keyid:(?:[0-9a-f]{2}(?::|$)){20}(?=$)/i),
-        isAlgorithms: _util.StringValidator(/^\S+$/),
-        isExpiration: (value) => _util.isInteger(value) && value > 0,
-        isPrivateKey: (value) => (value instanceof KeyObject) && value.type === 'private'
+        assert:           _util.Assert('ids.client.daps'),
+        isSKI:            _util.StringValidator(/^(?:[0-9a-f]{2}(?::|$)){20}(?=$)/i),
+        isAKI:            _util.StringValidator(/^keyid:(?:[0-9a-f]{2}(?::|$)){20}(?=$)/i),
+        isNonEmptyString: _util.StringValidator(/^\S+$/),
+        isExpiration:     (value) => _util.isInteger(value) && value > 0,
+        isPrivateKey:     (value) => (value instanceof KeyObject) && value.type === 'private',
+        isRequestAgent:   (value) => _util.isObject(value) && _util.isFunction(value.addRequest) && _util.isFunction(value.createConnection)
     },
     EventEmitter           = require('events'),
     {URL, URLSearchParams} = require('url'),
-    http                   = require('http'),
-    https                  = require('https'),
+    fetch                  = require('node-fetch'),
     {SignJWT}              = require('jose/jwt/sign'),
     {decode}               = require('jose/util/base64url');
 
+//region >> TYPEDEF
+/**
+ * @typedef {{"@context": "https://w3id.org/idsa/contexts/context.jsonld", "@type": "DatRequestPayload", iss: string, sub: string, aud: string, exp: number, nbf: number, iat: number}} DatRequestPayload
+ */ /**
+ * @typedef {string} DatRequestToken
+ */ /**
+ * @typedef {string} DatRequestQuery
+ */ /**
+ * @typedef {{"@context": "https://w3id.org/idsa/contexts/context.jsonld", "@type": "DatPayload", iss: string, sub: string, aud: string, exp: number, nbf: number, iat: number, scope: string | Array<string>, securityProfile: string, referringConnector?: string, transportCertsSha256?: string | Array<string>, extendedGuarantee?: string}} DatPayload
+ */ /**
+ * @typedef {string} DynamicAttributeToken
+ */
+//endregion >> TYPEDEF
+
 module.exports = class DAPSAgent extends EventEmitter {
 
-    #daps_url    = 'http://localhost:4567';
-    #private_key = null;
+    #daps_url      = 'http://localhost:4567';
+    #request_agent = null;
+    #private_key   = null;
 
     #assertion_algorithm  = 'RS256';
     #assertion_subject    = '';
@@ -28,28 +43,48 @@ module.exports = class DAPSAgent extends EventEmitter {
     #assertion_audience   = 'http://localhost:4567'; // 'idsc:IDS_CONNECTORS_ALL' | 'ALL'
     #assertion_scope      = 'IDS_CONNECTOR_ATTRIBUTES_ALL'; // 'idsc:IDS_CONNECTOR_ATTRIBUTES_ALL'
 
+    /**
+     * @param {Object} param
+     * @param {string} param.SKI
+     * @param {string} param.AKI
+     * @param {string} param.dapsUrl
+     * @param {KeyObject} param.privateKey
+     * @param {string} [param.algorithm]
+     * @param {number} [param.expiration]
+     * @param {{addRequest: Function, createConnection: Function}} [param.requestAgent]
+     */
     constructor(param) {
         util.assert(util.isObject(param), 'DAPSAgent#constructor : expected param to be an object', TypeError);
         util.assert(util.isSKI(param.SKI), 'DAPSAgent#constructor : expected param.SKI to be a string', TypeError);
         util.assert(util.isAKI(param.AKI), 'DAPSAgent#constructor : expected param.AKI to be a string', TypeError);
         util.assert(util.isString(param.dapsUrl), 'DAPSAgent#constructor : expected param.dapsUrl to be a string', TypeError);
-        util.assert(util.isPrivateKey(param.key), 'DAPSAgent#constructor : expected param.key to be a private KeyObject', TypeError);
-        util.assert(util.isNull(param.algorithm) || util.isAlgorithms(param.algorithm),
+        util.assert(util.isPrivateKey(param.privateKey), 'DAPSAgent#constructor : expected param.privateKey to be a private KeyObject', TypeError);
+        util.assert(util.isNull(param.algorithm) || util.isNonEmptyString(param.algorithm),
             'DAPSAgent#constructor : expected param.algorithm to be a nonempty string', TypeError);
         util.assert(util.isNull(param.expiration) || util.isExpiration(param.expiration),
             'DAPSAgent#constructor : expected param.expiration to be an integer greater than 0', TypeError);
+        util.assert(util.isNull(param.requestAgent) || util.isRequestAgent(param.requestAgent),
+            'DAPSAgent#constructor : expected param.requestAgent to be a request agent', TypeError);
 
         super();
 
         this.#daps_url          = param.dapsUrl;
         this.#assertion_subject = param.SKI + ':' + param.AKI;
-        this.#private_key       = param.key;
-        if (param.expiration) this. #assertion_expiration = param.expiration;
-        if (param.algorithm) this. #assertion_algorithm = param.algorithm;
+        this.#private_key       = param.privateKey;
+        if (param.expiration) this.#assertion_expiration = param.expiration;
+        if (param.algorithm) this.#assertion_algorithm = param.algorithm;
+        if (param.requestAgent) this.#request_agent = param.requestAgent;
     } // DAPSAgent#constructor
 
-    createDatRequestPayload(param) {
-        util.assert(util.isNull(param.expiration) || util.isExpiration(param.expiration),
+    // * @returns {{"@context": "https://w3id.org/idsa/contexts/context.jsonld", "@type": "DatRequestPayload", iss: string, sub: string, aud: string, exp: number, nbf: number, iat: number}}
+
+    /**
+     * @param {Object} [param]
+     * @param {number} [param.expiration]
+     * @returns {Promise<DatRequestPayload>}
+     */
+    async createDatRequestPayload(param) {
+        util.assert(util.isNull(param?.expiration) || util.isExpiration(param.expiration),
             'DAPSAgent#createDatRequestPayload : expected param.expiration to be an integer greater than 0', TypeError);
 
         const
@@ -60,7 +95,7 @@ module.exports = class DAPSAgent extends EventEmitter {
                 iss:        this.#assertion_subject,
                 sub:        this.#assertion_subject,
                 aud:        this.#assertion_audience,
-                exp:        now + (param.expiration ?? this.#assertion_expiration),
+                exp:        now + (param?.expiration ?? this.#assertion_expiration),
                 nbf:        now,
                 iat:        now
             };
@@ -68,27 +103,43 @@ module.exports = class DAPSAgent extends EventEmitter {
         return payload;
     } // DAPSAgent#createDatRequestPayload
 
-    async createClientAssertion(param) {
-        util.assert(util.isNull(param.algorithm) || util.isAlgorithms(param.algorithm),
-            'DAPSAgent#createClientAssertion : expected param.algorithm to be a nonempty string', TypeError);
+    /**
+     * @param {Object} [param]
+     * @param {number} [param.expiration]
+     * @param {string} [param.algorithm]
+     * @returns {Promise<DatRequestToken>}
+     */
+    async createDatRequestToken(param) {
+        util.assert(util.isNull(param?.algorithm) || util.isNonEmptyString(param.algorithm),
+            'DAPSAgent#createDatRequestToken : expected param.algorithm to be a nonempty string', TypeError);
 
         const
-            header          = {alg: param.algorithm ?? this.#assertion_algorithm},
-            payload         = this.createDatRequestPayload(param),
-            clientAssertion = await new SignJWT(payload)
+            header          = {alg: param?.algorithm ?? this.#assertion_algorithm},
+            payload         = await this.createDatRequestPayload(param),
+            datRequestToken = await new SignJWT(payload)
                 .setProtectedHeader(header)
                 .sign(this.#private_key);
 
-        return clientAssertion;
-    } // DAPSAgent#createClientAssertion
+        return datRequestToken;
+    } // DAPSAgent#createDatRequestToken
 
+    /**
+     * @param {Object} [param]
+     * @param {number} [param.expiration]
+     * @param {string} [param.algorithm]
+     * @param {DatRequestToken} [param.datRequestToken]
+     * @returns {Promise<DatRequestQuery>}
+     */
     async createDatRequestQuery(param) {
+        util.assert(util.isNull(param?.datRequestToken) || util.isNonEmptyString(param.datRequestToken),
+            'DAPSAgent#createDatRequestQuery : expected param.datRequestToken to be a nonempty string', TypeError);
+
         const
-            clientAssertion = await this.createClientAssertion(param),
+            datRequestToken = param?.datRequestToken ?? await this.createDatRequestToken(param),
             queryParams     = {
                 grant_type:            'client_credentials',
                 client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-                client_assertion:      clientAssertion,
+                client_assertion:      datRequestToken,
                 scope:                 this.#assertion_scope
             },
             requestQuery    = new URLSearchParams(queryParams).toString();
@@ -96,31 +147,78 @@ module.exports = class DAPSAgent extends EventEmitter {
         return requestQuery;
     } // DAPSAgent#createDatRequestQuery
 
+    /**
+     * @param {Object} [param]
+     * @param {number} [param.expiration]
+     * @param {string} [param.algorithm]
+     * @param {DatRequestToken} [param.datRequestToken]
+     * @param {DatRequestQuery} [param.datRequestQuery]
+     * @returns {Promise<{url: string, method: string, headers: {[key: string]: string}, body: DatRequestQuery}>}
+     */
     async createDatRequest(param) {
+        util.assert(util.isNull(param?.datRequestQuery) || util.isNonEmptyString(param.datRequestQuery),
+            'DAPSAgent#createDatRequest : expected param.datRequestQuery to be a nonempty string', TypeError);
+
         const
             requestUrl = new URL('/token', this.#daps_url).toString(),
             request    = {
                 url:     requestUrl,
                 method:  'POST',
                 headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-                body:    await this.createDatRequestQuery(param)
+                body:    param?.datRequestQuery ?? await this.createDatRequestQuery(param)
             };
+
+        if (!this.#request_agent) request.agent = this.#request_agent;
 
         return request;
     } // DAPSAgent#createDatRequest
 
-    async fetchAccessToken(param) {
+    /**
+     * @param {Object} [param]
+     * @param {number} [param.expiration]
+     * @param {string} [param.algorithm]
+     * @param {DatRequestToken} [param.datRequestToken]
+     * @param {DatRequestQuery} [param.datRequestQuery]
+     * @returns {Promise<DynamicAttributeToken>}
+     */
+    async fetchDat(param) {
         const
             request  = await this.createDatRequest(param),
             response = await fetch(request.url, request);
 
-        util.assert(response.ok, 'DAPSAgent#fetchAccessToken : [' + response.status + '] ' + response.statusText);
+        util.assert(response.ok, 'DAPSAgent#fetchDat : [' + response.status + '] ' + response.statusText);
 
         const
-            result      = await response.json(),
-            accessToken = result.access_token;
+            result                = await response.json(),
+            dynamicAttributeToken = result.access_token;
 
-        return accessToken;
-    } // DAPSAgent#fetchAccessToken
+        util.assert(util.isNonEmptyString(dynamicAttributeToken), 'DAPSAgent#fetchDat : expected dynamicAttributeToken to be a non empty string');
+
+        return dynamicAttributeToken;
+    } // DAPSAgent#fetchDat
+
+    async fetchJwks(param) {
+        const
+            requestUrl = new URL('/.well-known/jwks.json', this.#daps_url).toString(),
+            response   = await fetch(requestUrl);
+
+        util.assert(response.ok, 'DAPSAgent#fetchJwks : [' + response.status + '] ' + response.statusText);
+
+        const
+            jwks = await response.json();
+
+        util.assert(util.isArray(jwks?.keys), 'DAPSAgent#fetchJwks : expected jwks to have a keys array');
+
+        return jwks;
+    } // DAPSAgent#fetchJWKS
+
+    /**
+     * @param {DynamicAttributeToken} dynamicAttributeToken
+     * @returns {Promise<DatPayload>}
+     */
+    async validateDat(dynamicAttributeToken) {
+        util.assert(util.isNonEmptyString(dynamicAttributeToken), 'DAPSAgent#validateDat : expected dynamicAttributeToken to be a non empty string');
+        // TODO
+    } // DAPSAgent#validateDat
 
 };
--- next/ids.client.daps.test.js
+++ next/ids.client.daps.test.js
@@ -7,15 +7,44 @@ const
 
 describe('ids.client.daps', function () {
 
+    this.timeout(10e3);
+
     test('exported module should be a class and instantiable', function () {
         expect(typeof DAPSClient).toBe('function');
         const dapsClient = new DAPSClient({
-            dapsUrl: 'https://localhost:8081',
-            SKI:     'DD:CB:FD:0B:93:84:33:01:11:EB:5D:94:94:88:BE:78:7D:57:FC:4A',
-            AKI:     'keyid:CB:8C:C7:B6:85:79:A8:23:A6:CB:15:AB:17:50:2F:E6:65:43:5D:E8',
-            key:     crypto.createPrivateKey(certs.client.private)
+            dapsUrl:    'http://localhost:4567',
+            SKI:        'DD:CB:FD:0B:93:84:33:01:11:EB:5D:94:94:88:BE:78:7D:57:FC:4A',
+            AKI:        'keyid:CB:8C:C7:B6:85:79:A8:23:A6:CB:15:AB:17:50:2F:E6:65:43:5D:E8',
+            privateKey: crypto.createPrivateKey(certs.client.private)
         });
         expect(dapsClient).toBeInstanceOf(DAPSClient);
+        console.log(dapsClient);
     }); // test
+
+    test('the daps client should be able to fetch a new dat', async function () {
+        const
+            dapsClient = new DAPSClient({
+                dapsUrl:    'http://localhost:4567',
+                SKI:        'DD:CB:FD:0B:93:84:33:01:11:EB:5D:94:94:88:BE:78:7D:57:FC:4A',
+                AKI:        'keyid:CB:8C:C7:B6:85:79:A8:23:A6:CB:15:AB:17:50:2F:E6:65:43:5D:E8',
+                privateKey: crypto.createPrivateKey(certs.client.private)
+            }),
+            dat        = await dapsClient.fetchDat();
+        expect(typeof dat).toBe('string');
+        console.log(dat);
+    });
+
+    test('the daps client should be able to fetch the jwks', async function () {
+        const
+            dapsClient = new DAPSClient({
+                dapsUrl:    'http://localhost:4567',
+                SKI:        'DD:CB:FD:0B:93:84:33:01:11:EB:5D:94:94:88:BE:78:7D:57:FC:4A',
+                AKI:        'keyid:CB:8C:C7:B6:85:79:A8:23:A6:CB:15:AB:17:50:2F:E6:65:43:5D:E8',
+                privateKey: crypto.createPrivateKey(certs.client.private)
+            }),
+            jwks       = await dapsClient.fetchJwks();
+        expect(Array.isArray(jwks?.keys)).toBeTruthy();
+        console.log(jwks);
+    });
 
 }); // describe
